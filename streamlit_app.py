0a61eb0af973ecf67bc4a64b76652081146ad4d2
lauralad <98854129+lauralad@users.noreply.github.com>
2024-04-23 20:28:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,11 +73,11 @@ def clean_json_file(file_path):
 
 def load_csv_data(file_path):
     # Read the CSV using Pandas and extract demo names and turn numbers
-    pred_df = pd.read_csv(file_path)
-    # for index, row in df.iterrows():
-    #     turn_number = row['turn']
-    #     data_mapping[turn_number] = index
-    return pred_df[['demo', 'turn']]
+    df = pd.read_csv(file_path)
+    for index, row in df.iterrows():
+        turn_number = row['turn']
+        data_mapping[turn_number] = index
+    # return pred_df[['demo', 'turn']]
 
 def create_mapping(json_data, csv_df):
     # Create a DataFrame linking JSON entries to demo and turn info
@@ -88,12 +88,12 @@ def create_mapping(json_data, csv_df):
     return csv_df
 
 def setup_datasets():
-    global unique_data_dict, cleaned_data,data_mapping
+    global unique_data_dict, cleaned_data, data_mapping
     file_path = './valid_predictions.json'
     cleaned_data = clean_json_file(file_path)
-    st.write(cleaned_data)
+    # st.write(cleaned_data)
     csv_df = load_csv_data("./valid.csv")
-    st.write(csv_df)
+    # st.write(csv_df)
     data_mapping = create_mapping(cleaned_data, csv_df)
     st.write(data_mapping)
     unique_data_dict = load_and_prepare_data()
@@ -278,8 +278,9 @@ def show_overview(data, recording_name, dataset, demo_name, turn, basedir):
         # col_act.markdown(action_str)
         col_act1.markdown(action_str)
         if i == turn:
-            pred_action = "hi"
-            col_act2.markdown(action_str)
+            pred_idx = data_mapping[turn]
+            pred_action = cleaned_data[pred_idx]
+            col_act2.markdown(pred_action)
         else:
             col_act2.markdown(action_str)#predicted_action_str
 
