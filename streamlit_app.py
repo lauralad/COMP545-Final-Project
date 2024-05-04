36246d9f5fc7fef42a9f9cfaa972ce7e31d3adb4
lauralad <98854129+lauralad@users.noreply.github.com>
2024-05-03 20:14:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -611,6 +611,7 @@ def show_overview(data, model_name, recording_name, dataset, demo_name, turn, ba
             key = f"{demo_name}_{turn}"
             pred_idx = data_mapping[key]
             pred_action = cleaned_data[0][pred_idx]
+            st.write(f"Predicted Action: {pred_action}")
             col_act2.markdown(pred_action)
             # col_act2.markdown(action_str)
         else:
